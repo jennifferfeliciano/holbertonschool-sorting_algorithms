c551e8416ebe07fda3d45f94bcd6218a6898c1f8
carlfrank <carl-frank7@hotmail.com>
2023-09-15 17:04:04 -0400

    Quick, 333

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,93 +1,89 @@
-#include <stdio.h>
 #include "sort.h"
 
 /**
- * swap - Swaps two elements in an array.
- * @array: The array containing elements to be swapped.
- * @a: Index of the first element.
- * @b: Index of the second element.
- */
-void swap(int *array, size_t a, size_t b)
+ * _swap - Swap two numbers.
+ * @a: Integer
+ * @b: Integer
+ **/
+
+void _swap(int *a, int *b)
 {
-    int temp = array[a];
-    array[a] = array[b];
-    array[b] = temp;
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+
 }
 
 /**
- * partition - Lomuto partition scheme for Quick Sort.
- * @array: The array to be partitioned.
- * @low: The starting index of the partition.
- * @high: The ending index of the partition.
- * @size: The size of the array.
- *
- * Return: The index of the pivot element after partitioning.
- */
-size_t partition(int *array, ssize_t low, ssize_t high, size_t size)
+ * _split - Split the array and takes the last element as pivot
+ * @arr: Array
+ * @min: First element
+ * @last: Last element
+ * @size: Size
+ * Return: Integer
+ **/
+int _split(int *arr, int min, int last, size_t size)
 {
-    int pivot = array[high];
-    ssize_t i = low - 1;
+	int piv;
+	int i = (min);
+	int j;
 
-    for (ssize_t j = low; j <= high - 1; j++)
-    {
-        if (array[j] <= pivot)
-        {
-            i++;
-            if (i != j)
-            {
-                swap(array, i, j);
-                printf("Swap: %d %d\n", array[i], array[j]);
-                for (size_t k = 0; k < size; k++)
-                {
-                    if (k != 0)
-                        printf(", ");
-                    printf("%d", array[k]);
-                }
-                printf("\n");
-            }
-        }
-    }
-    if (i + 1 != high)
-    {
-        swap(array, i + 1, high);
-        printf("Swap: %d %d\n", array[i + 1], array[high]);
-        for (size_t k = 0; k < size; k++)
-        {
-            if (k != 0)
-                printf(", ");
-            printf("%d", array[k]);
-        }
-        printf("\n");
-    }
-    return (i + 1);
+	piv = arr[last];
+	for (j = min; j < last; j++)
+	{
+		if (arr[j] <= piv)
+		{
+
+			_swap(&arr[i], &arr[j]);
+
+
+			if (i != j)
+				print_array(arr, size);
+
+			i++;
+
+		}
+	}
+
+	_swap(&arr[i], &arr[last]);
+	if (i != j)
+		print_array(arr, size);
+
+	return (i);
 }
 
 /**
- * quicksort - Recursive function to perform Quick Sort.
- * @array: The array to be sorted.
- * @low: The starting index of the partition.
- * @high: The ending index of the partition.
- * @size: The size of the array.
+ * quick_sort_array - The quick short array
+ * @arr: arr
+ * @min: min
+ * @last: last
+ * @size: size
+ * Return: Nothing
  */
-void quicksort(int *array, ssize_t low, ssize_t high, size_t size)
+void quick_sort_array(int *arr, int min, int last, size_t size)
 {
-    if (low < high)
-    {
-        size_t pi = partition(array, low, high, size);
-        quicksort(array, low, pi - 1, size);
-        quicksort(array, pi + 1, high, size);
-    }
+
+	int piv;
+
+	if (min < last)
+	{
+		piv = _split(arr, min, last, size);
+		quick_sort_array(arr, min, (piv - 1), size);
+		quick_sort_array(arr, (piv + 1), last, size);
+	}
 }
 
 /**
- * quick_sort - Sorts an array of integers in ascending order using Quick Sort.
- * @array: The array to be sorted.
- * @size: The size of the array.
- */
+ * quick_sort -Sort an array using quick_sort algorithm
+ * @array: array
+ * @size: size
+ **/
 void quick_sort(int *array, size_t size)
 {
-    if (array == NULL || size < 2)
-        return;
+	if (size < 2)
+		return;
 
-    quicksort(array, 0, size - 1, size);
+	quick_sort_array(array, 0, size - 1, size);
 }
